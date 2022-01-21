c0102761ce68b7df69fbaee13b808ef4cbd8dd47
Jane0705 <janeli0705@gmail.com>
2022-01-21 23:08:20 +0800

    GameOfLifeUtility Script

---
+++ AI_Game_of_Life/Assets/Scripts/GameOfLifeUtility.cs
@@ -0,0 +1,9 @@
+public static class GameOfLifeUtility
+{
+    public static bool DecideNextState(bool cellLivingState, int aliveNeighbourCount)
+    {
+        bool result = false;
+        //Finish logic here
+        return result;
+    }
+}
